d030f00270078977383e411e45329cd48f5d23c3
ShaniBhai786 <roshaanahmad91@gmail.com>
2026-03-02 19:29:47 +0500

    Implement User Login form structure in UserLoging component

--- client/src/components/UserLoging.jsx
+++ client/src/components/UserLoging.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 function UserLoging() {
   return (
     <div className='user-container'>
-      
+      <h1>User Login</h1>
+      <Formik>
+        <Form>
+            
+        </Form>
+      </Formik>
     </div>
   )
 }
